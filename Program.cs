a7c0501b848efea47e17c7a35c51337e1a91a45b
megastary <megastary@gmail.com>
2017-01-25 23:48:14 +0100

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NextSentenceBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyBot bot = new MyBot();
+        }
+    }
+
+}
